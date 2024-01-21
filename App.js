7d2c965ccd888d62a9f15ad8c6e1f2c545a4b400
Esther2524 <wangzhixiao2020@163.com>
2024-01-21 14:49:37 -0800

    Modified code

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
+
 import Header from './components/Header';
+import Input from './components/Input';
 
 export default function App() {
   const appName = "My App"
+  const [age, setAge] = useState(42);
+  const [text, setText] = useState("");
+
+
   return (
     <View style={styles.container}>
+
       <Text>Welcome to {appName} 🐶</Text>
       <StatusBar style="auto" />
+      <Header name="My App" version={2} />
+      <Input />
 
-      <Header name="My App" version={2}>
-        <Text>Child</Text>
-      </Header>
-      
     </View>
   );
 }
@@ -24,4 +30,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
