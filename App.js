8c918aa19cff4a0d052d8e2a90633ff990b74117
Esther2524 <wangzhixiao2020@163.com>
2024-01-23 18:59:44 -0800

    Added code used in week3

--- App.js
+++ App.js
@@ -1,27 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { useState } from 'react';
 
 import Header from './components/Header';
 import Input from './components/Input';
+import { SafeAreaView } from 'react-native';
 
 export default function App() {
   const appName = "My App"
-  const [age, setAge] = useState(42);
-  const [text, setText] = useState("");
+
+  const [data, setData] = useState("")
+  const [isVisible, setIsVisible] = useState(false)
+
+  function receiveInput(data) {
+    console.log("receive input from Input.js", data);
+    setData(data);
+    setIsVisible(false);
+  }
+
+  function dismissModal() {
+    setIsVisible(false);
+  }
+
 
 
   return (
-    <View style={styles.container}>
 
-      <Text>Welcome to {appName} 🐶</Text>
-      <StatusBar style="auto" />
-      <Header name="My App" version={2} />
-      <Input />
+    <SafeAreaView style={styles.container}>
 
-    </View>
+      <View style={styles.topView}>
+        <Text>Welcome to {appName} 🐶</Text>
+        <StatusBar style="auto" />
+        <Header name="My App" version={2} />
+
+        <Button title="add a goal" onPress={() => setIsVisible(true)} />
+        <Input inputHandler={receiveInput} modalVisible={isVisible} dismissModal={dismissModal} />
+
+      </View>
+
+      <View style={styles.bottomView}>
+
+
+        <Text>{data}</Text>
+
+
+      </View>
+
+    </SafeAreaView>
+
+
+
+
+
+
+
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -29,6 +64,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  topView: {
+    flex: 1,
+  },
+
+  bottomView: {
+    flex: 4,
+    backgroundColor: "green",
+  },
+
+  text: {
+    textAlign: "center",
   },
 
 });
