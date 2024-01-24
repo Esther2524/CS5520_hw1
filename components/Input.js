2c36768b381eb44289f57575280367ecb039aa4d
Esther2524 <wangzhixiao2020@163.com>
2024-01-23 19:03:45 -0800

    Added code used in week3

--- components/Input.js
+++ components/Input.js
@@ -1,70 +1,89 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native'
+import { StyleSheet, TextInput, View, Button, Modal, Image } from 'react-native'
 import React, { useState } from 'react'
 
-export default function Input() {
-  
-  // a state variable named `age`, and a function to update it called `setAge`.
-  const [age, setAge] = useState("24");
-  const [name, setName] = useState("Esther")
 
+export default function Input({ inputHandler, modalVisible, dismissModal }) {
 
-  // This is a handler function that gets called whenever the text in the TextInput changes.
-  // The changedText parameter represents the "latest value" of the text input field.
-  // Inside this function, setText(changedText) updates the state variable text with the latest value from the input field.
-  function changeNameHandler(changedName) {
-    setName(changedName)
+  const [text, setText] = useState("")
+
+  //callback handler
+  function changeTextHandler(changedText) {
+    console.log("user is typing:", changedText)
+    setText(changedText)
   }
 
-  function changeAgeHandler(changedAge) {
-    // console.log("User is typing:", changedAge);
-    setAge(changedAge)
+
+  function confirmHandler() {
+    inputHandler(text);
+
+  }
+
+  function cancelHandler() {
+    // hide the model
+    dismissModal()
   }
 
 
 
   return (
-    // The line value={text} in the TextInput component is a crucial part of managing form inputs
-    // The text variable (initialized and managed by the useState hook) holds the current value of the input. 
-    // This means the actual value displayed in the TextInput field is always the same as the text state variable.
+    <Modal visible={modalVisible}>
+      <View style={styles.container}>
+        <Image
+          style={styles.image}
+          source={
+            require("../assets/google_img.png")
+          }
+        />
+    
+        <Image />
 
-    // value={text} binds the value of the input field to the `text` state variable. 
-    // This means the input field displays whatever value `text` holds.
-    <View>
-      
+        <TextInput
+          placeholder='Type something'
+          style={styles.input}
+          value={text}
+          onChangeText={changeTextHandler} />
+
+        <View style={styles.buttonContainer}>
+
+   
+          <Button style={{}} title="Cancel" onPress={cancelHandler} />
+          <Button title="Submit" onPress={confirmHandler} />
+        </View>
 
 
-      <Text>Enter Name:</Text>
-      <TextInput
-        placeholder='e.g. Esther'
-        style={styles.input}
-        onChangeText={(val) => setName(val)}
-      />
 
-      {/* another format */}
-      <TextInput placeholder="e.g. Esther"
-        style={styles.input}
-        value={name}
-        onChangeText={changeNameHandler} />
+      </View>
 
-      <Text>Enter Age:</Text>
-      <TextInput 
-        placeholder='e.g. 24'
-        style={styles.input}
-        onChangeText={(val) => setAge(val)}
-      />
+    </Modal>
 
-      <Text>Name: {name}, Age: {age}</Text>
-    </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   input: {
     borderWidth: 1,
     borderColor: "#777",
     padding: 8,
     margin: 10,
     width: 200,
+  },
+  image: {
+    width: "95%",
+    height: "30%",
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  buttonView: {
+
   }
+
 })
 
