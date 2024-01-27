ee4afc64c0412f2931a6cc10cb965f1134b64a45
Esther2524 <wangzhixiao2020@163.com>
2024-01-26 22:55:14 -0800

    Centralized color definitions

---
+++ Colors.js
@@ -0,0 +1,18 @@
+const Colors = {
+    text: '#492E87',
+    border: '#492E87',
+    normalButton: 'blue',
+    doneButton: '#D04848',
+    resetButton: '#D04848',
+    confirmButton: '#492E87',
+    disabledButton: 'white',
+    shadow: '#333',
+    linearColorStart: '#F5EEE6',
+    linearColorEnd: '#E6A4B4',
+    card:'#E0CCBE',
+    checkBox: '#4630EB',
+    errorMessage: 'black'
+    
+};
+
+export default Colors;
