94c54d72a2b0b29c2470cd6645814a94a92b8af6
Esther2524 <wangzhixiao2020@163.com>
2024-01-29 13:27:03 -0800

    Moved GameScreen previously in StartScreen in App.js

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import { StyleSheet, View } from 'react-native';
 import { useState } from 'react';
 import StartScreen from './screens/StartScreen';
 import FinishScreen from './screens/FinishScreen';
+import GameScreen from './screens/GameScreen';
 import LinearGradientWrapper from './components/LinearGradientWrapper';
 
 export default function App() {
   // notes:
-  // These state variables are shared between the StartScreen and FinishScreen.
-  // The StartScreen is responsible for allowing the user to interact and modify these state variables, 
-  // while the FinishScreen needs access to these variables to display results and allow restarting the game.
+  // These state variables are shared between the StartScreen, GameScreen, and FinishScreen.
+  // the FinishScreen needs access to these variables to display results and allow restarting the game.
   const [userName, setUserName] = useState('');
   const [userNumber, setUserNumber] = useState('');
   const [attemptsLeft, setAttemptsLeft] = useState(3);
@@ -16,18 +16,68 @@ export default function App() {
     Math.floor(Math.random() * 10) + 1020
   );
   const [currentScreen, setCurrentScreen] = useState('StartScreen'); // Screen state
-  const [isGameWon, setIsGameWon] = useState(false)
+  // for GameScreen
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isGuessHigher, setIsGuessHigher] = useState(false)
+  const [isGuessCorrect, setIsGuessCorrect] = useState(false)
+  // for FinishScreen
+  const [isGameWon, setIsGameWon] = useState(false);
+  
+  // console.log(generatedNumber); // just for test
+
+  // the logic of the entire game
+  function checkGuess() {
+    console.log(`user input: ${userNumber}, correct number: ${generatedNumber}`); // just for test
+    const num = parseInt(userNumber, 10);
+    if (num === generatedNumber) {
+      setIsGuessCorrect(true);
+      setIsGameWon(true);
+    } else {
+      setIsGuessCorrect(false);
+      if (num > generatedNumber) {
+        setIsGuessHigher(true);
+      } else {
+        setIsGuessHigher(false);
+      }
+      // each time Confirm button is clicked, adjust attemptsLeft (decrement by 1)          
+      setAttemptsLeft(attemptsLeft - 1);
+    }
+  };
+
+  // For StartScreen:
+  // dislay the GameScreen Modal
+  function displayModal() {
+    setIsModalVisible(true);
+  }
+
+  // For GameScreen: 
+  // handle "Let Me Guess Again" Button (back to StartScreen)
+  function handleGuessAgain() {
+    setIsModalVisible(false);
+    setCurrentScreen('StartScreen');
+  }
+
+  // For GameScreen:
+  // handle "I'm done" Button. when the game is over, go to the FinishScreen
+  // two situations: 1. the user enters the correct number; 2. exceed the maximum attempts
+  function handleDone() {
+    setIsModalVisible(false);
+    setCurrentScreen('FinishScreen');
+  }
 
 
-  // restart the entire game
+  // For FinishScreen: restart the entire game
   function restartGame() {
     setUserName('');
     setUserNumber('');
     setGeneratedNumber(Math.floor(Math.random() * 10) + 1020);
     setAttemptsLeft(3);
+    setIsGuessCorrect(false);
     setIsGameWon(false);
     setCurrentScreen('StartScreen');
   };
+
+
 
   return (
     <LinearGradientWrapper>
@@ -38,13 +88,22 @@ export default function App() {
             setUserName={setUserName}
             userNumber={userNumber}
             setUserNumber={setUserNumber}
-            attemptsLeft={attemptsLeft}
-            setAttemptsLeft={setAttemptsLeft}
-            generatedNumber={generatedNumber}
-            setCurrentScreen={setCurrentScreen}
-            setIsGameWon={setIsGameWon}
+            checkGuess={checkGuess}
+            displayModal={displayModal}
           />
         )}
+
+        {/* Place GameScreen in the App.js, not conditionally render it. */}
+        <GameScreen
+          isModalVisible={isModalVisible}
+          userName={userName}
+          userNumber={userNumber}
+          attemptsLeft={attemptsLeft}
+          isGuessCorrect={isGuessCorrect}
+          isGuessHigher={isGuessHigher}
+          handleDone={handleDone}
+          handleGuessAgain={handleGuessAgain}
+        />
 
         {currentScreen == 'FinishScreen' && (
           <FinishScreen
@@ -52,6 +111,7 @@ export default function App() {
             handleRestart={restartGame}
             isGameWon={isGameWon} />
         )}
+
       </View>
     </LinearGradientWrapper>
   );
