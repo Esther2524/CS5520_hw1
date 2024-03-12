9b6e23568bea28bf4f9dc7a94f0bbc0bb4a254a1
Esther2524 <wangzhixiao2020@163.com>
2024-03-12 13:46:37 -0700

    Added code for week6

--- firebase-files/firebaseSetup.js
+++ firebase-files/firebaseSetup.js
@@ -5,6 +5,10 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId } from "@env";
 
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -19,3 +23,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
+
+
+// export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
