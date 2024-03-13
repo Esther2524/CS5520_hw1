b8fb1da840f000207696382309c271567fb00639
Esther2524 <wangzhixiao2020@163.com>
2024-03-12 18:19:36 -0700

    Added code for Sign up and Log in

---
+++ Theme.js
@@ -0,0 +1,51 @@
+export const Colors = {
+    text: '#12372A',
+    border: '#436850',
+    startButton: '#12372A',
+    initialStartButton: '#FBFADA',
+    resetButton: '#D04848',
+    cancelButton: '#944E63',
+    saveButton: '#436850',
+    buttonTitle: 'white',
+    disabledButton: '#E3E1D9',
+    androidRipple: 'lightgrey',
+    shadow: '#333',
+    errorMessage: '#12372A',
+    header: '#E1F0DA',
+    headerBackground: '#436850',
+    headerButton: 'white',
+    addButton: '#E1F0DA',
+    activeTab: '#E8C872',
+    inactiveTab: '#FBFADA',
+    screen: '#ADBC9F',
+    dropdownBackground: '#D4E7C5',
+    addText: '#294B29',
+    itemBackground: '#436850',
+    itemTexBackground: '#FDF0D1',
+    itemName: 'white',
+    checkBox: '#436850',
+    checkBoxLabel: '#12372A',
+    deleteButton: '#FFF8E3',
+};
+
+export const fontSize = {
+    normalText: 16,
+    inputText: 18,
+    errorText: 16,
+    buttonText: 17,
+    activityText: 16,
+    labelText: 16,
+    checkBox: 14,
+};
+
+export const Padding = {
+    itemPadding: 20,
+    activityPadding: 5,
+    containerPadding: 10,
+    inputPadding: 8,
+    textInputPadding: 10,
+    checkBoxPadding: 5,
+    buttonDefaultPadding: 1,
+    buttonPadding: 8,
+}
+
--- firebase-files/firebaseSetup.js
+++ firebase-files/firebaseSetup.js
@@ -3,13 +3,17 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from "firebase/firestore";
-import { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId } from "@env";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-
-
-
+import {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+} from "@env";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: apiKey,
@@ -23,9 +27,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
-
-
 // export const auth = getAuth(app);
 export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
